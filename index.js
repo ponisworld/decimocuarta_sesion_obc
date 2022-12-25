fbdf49b1d303e0275e70afae1bed401a33365d05
Andrea Bellver <bellverrha@gmail.com>
2022-12-25 07:38:35 +0100

    Prueba JQuery con el botón

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', () => {alert('Click en el botón')});
+
+$("button").click(() => { console.log('Hola! Estoy utilizando JQuery.') })
